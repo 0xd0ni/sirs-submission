2cf7fd7fef5490e77a604e6f2013646e1e25e11d
Stani-s <106707078+Stani-s@users.noreply.github.com>
2023-11-27 11:09:54 +0000

    Criação de projeto maven e parsing de opções

---
+++ MediTrack/src/main/java/tecnico/a01/App.java
@@ -0,0 +1,45 @@
+package tecnico.a01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        if (args.length != 2) {
+            printUsage();
+        } else if (args.length == 2){
+            if (args[0].equals("protect")) {
+                System.out.println("Protecting file " + args[1]);
+                // Create Digest
+                // Encrypt with public key
+                // Encode to Base64
+
+            } else if (args[0].equals("unprotect")) {
+                System.out.println("Unprotecting file " + args[1]);
+                // Decode from Base64
+                // Decrypt with private key
+                // Compare Digest ?
+
+            } else if (args[0].equals("check")) {
+                System.out.println("Checking file " + args[1]);
+                // Decode from Base64
+                // Decrypt with private key
+                // Recreate Digest
+                // Compare Digest
+
+            } else {
+                printUsage();
+            }
+        }
+    }
+
+    public void printUsage() {
+        System.out.println("Usage:");
+        System.out.println("protect *filename*");
+        System.out.println("unprotect *filename*");
+        System.out.println("check *filename*");
+    }
+}
