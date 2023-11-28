51f9d796d49c42ec2b0227f76820e761697254a8
ist197281 <allancravid@tecnico.ulisboa.pt>
2023-11-28 17:10:22 +0000

    MediTrack: remove bare testing for now

--- MediTrack/src/test/java/pt/tecnico/a01/AppTest.java
+++ MediTrack/src/test/java/pt/tecnico/a01/AppTest.java
@@ -1,20 +1,9 @@
 package pt.tecnico.a01;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
 }
