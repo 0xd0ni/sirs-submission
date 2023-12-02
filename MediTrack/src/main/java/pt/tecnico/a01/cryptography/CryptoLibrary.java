5e8a17cac8861f33d89dd9ff14206c9df2a77bea
diogosil01 <diogo@LAPTOP-2T9QL9DC.localdomain>
2023-12-02 10:08:16 +0000

    encryption implementation

--- MediTrack/src/main/java/pt/tecnico/a01/cryptography/CryptoLibrary.java
+++ MediTrack/src/main/java/pt/tecnico/a01/cryptography/CryptoLibrary.java
@@ -1,5 +1,15 @@
 package pt.tecnico.a01.cryptography;
-
+import java.io.*;
+import java.util.*;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import javax.crypto.Cipher;
+import java.security.Key;
+import javax.crypto.KeyGenerator;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 
 public class CryptoLibrary {
@@ -9,8 +19,33 @@ public class CryptoLibrary {
     //  Main operations
     // --------------------------------------------------------------------------------------------
     
-    public static void protect() {
-   
+    public static void protect(String file) throws Exception{
+        final String filename = file;
+        // Read  MediTrack JSON object from file, and print its contents
+        try (FileReader fileReader = new FileReader(filename)) {
+            Gson gson = new Gson();
+            JsonObject rootJson = gson.fromJson(fileReader, JsonObject.class);
+            System.out.println("JSON object: " + rootJson);
+            JsonObject patientObject = rootJson.get("patient").getAsJsonObject();
+            String[] aes_fields = {"name","sex"};
+            String[] rsa_fields = {"dateOfBirth","bloodType","knownAllergies","consultationRecords"};
+            for (String field: aes_fields)
+            {
+                byte[] bytes = patientObject.get(field).getAsString().getBytes();
+                byte[] encryptedBytes = aes_encrypt(bytes);
+                String encryptedBase64 = Base64.getEncoder().encodeToString(encryptedBytes);
+                patientObject.addProperty(field, encryptedBase64);
+            }
+
+            for (String field: rsa_fields)
+            {
+                byte[] bytes = patientObject.get(field).getAsString().getBytes();
+                byte[] encryptedBytes = rsa_encrypt(bytes);
+                String encryptedBase64 = Base64.getEncoder().encodeToString(encryptedBytes);
+                patientObject.addProperty(field, encryptedBase64);
+            }
+
+        }
     }
 
 
@@ -26,7 +61,31 @@ public class CryptoLibrary {
     // --------------------------------------------------------------------------------------------
     //  Utilities
     // --------------------------------------------------------------------------------------------
+    public static byte[] aes_encrypt(byte[] bytes) throws Exception{
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+		keyGen.init(128);
+		Key key = keyGen.generateKey();
+        // cipher data
+        final String CIPHER_ALGO = "AES/aes/PKCS5Padding";
+        System.out.println("Ciphering with " + CIPHER_ALGO + "...");
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGO);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] cipherBytes = cipher.doFinal(bytes);
+        return cipherBytes;
+    }
 
+    public static byte[] rsa_encrypt(byte[] bytes) throws Exception{
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(2048);
+		KeyPair keyPair = keyGen.generateKeyPair();
+        // cipher data
+        final String CIPHER_ALGO = "RSA/aes/PKCS1Padding";
+        System.out.println("Ciphering with " + CIPHER_ALGO + "...");
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGO);
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+        byte[] cipherBytes = cipher.doFinal(bytes);
+        return cipherBytes;
+    }
 
 
 
