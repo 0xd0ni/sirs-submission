d6392c6dd74b15189eda56c27a0b5d1cd111924a
ist195552 <ist19552@tecnico.ulisboa.pt>
2023-12-16 13:26:37 +0000

    firewall rules

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 setxkbmap -model abnt2 -layout pt
+sudo /sbin/iptables -F
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A INPUT -s  192.168.56.11 -j ACCEPT
 sudo ifconfig eth0 192.168.56.11/24 up
