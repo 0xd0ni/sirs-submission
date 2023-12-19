f2bccddf3b42edf3b46fb868759e5c8328f19757
ist195552 <ist19552@tecnico.ulisboa.pt>
2023-12-19 11:46:28 +0000

    mongodb version fix

--- scripts/bootstrap_db.sh
+++ scripts/bootstrap_db.sh
@@ -5,11 +5,11 @@ sudo apt-get update -y
 sudo apt-get -y install maven
 sudo apt-get -y install openssl
 sudo apt-get -y install default-jdk
-curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-2.2.7.tgz
-tar -zxvf mongodb-linux-x86_64-2.2.7.tgz
+curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-3.6.1.tgz
+tar -zxvf mongodb-linux-x86_64-3.6.1.tgz
 mkdir -p mongodb
-cp -R -n mongodb-linux-x86_64-2.2.7/ mongodb
+cp -R -n mongodb-linux-x86_64-3.6.1/ mongodb
 mkdir -p mongodb/data/db
-rm -r mongodb-linux-x86_64-2.2.7
-rm -rf mongodb-linux-x86_64-2.2.7.tgz
-# para correr o mongo: sudo ./mongodb/bin/mongod --dbpath ./mongodb/data/db
+rm -r mongodb-linux-x86_64-3.6.1
+rm -rf mongodb-linux-x86_64-3.6.1.tgz
+# para correr o mongodb: sudo ./mongodb/bin/mongod --dbpath ./mongodb/data/db
--- scripts/db.sh
+++ scripts/db.sh
@@ -3,4 +3,8 @@ setxkbmap -model abnt2 -layout pt
 sudo /sbin/iptables -F
 sudo iptables -A INPUT -s  192.168.56.11 -j ACCEPT
 sudo iptables -A INPUT -i icmp -j DROP
+#openssl genrsa -out db.key  
+#openssl req -new -key db.key -out db.csr
+#openssl x509 -req -days 365 -in db.csr -signkey db.key -out db.crt
+#openssl x509 -in db.crt -out db.pem
 
